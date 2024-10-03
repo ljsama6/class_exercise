5c2ac23e4841a5ff8872c83da0227e6e982a3510
ljsama <2817982520@qq.com>
2024-10-03 15:00:55 +0800

    可变参数

---
+++ ClassExercise23.java
@@ -0,0 +1,19 @@
+package class_exercise;
+
+//可变参数应用
+class HspMethod {
+    public String showScore(String name, int... score) {
+        int res = 0;
+        for (int i = 0; i < score.length; i++) {
+            res += score[i];
+        }
+        return "姓名:" + name + " " + score.length + "门" + "成绩总分:" + res;
+    }
+}
+
+public class ClassExercise23 {
+    public static void main(String[] args) {
+        HspMethod a = new HspMethod();
+        System.out.println(a.showScore("张三", 12, 20, 30, 50 ));
+    }
+}
